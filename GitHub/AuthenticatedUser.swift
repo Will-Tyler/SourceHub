7edb8caa234e4625c578a209e5f1a14ec87e90ec
Will Tyler <will.tyler11@gmail.com>
2019-04-05 23:33:10 +0000

    Expand AuthenticatedUser struct

--- GitHub/AuthenticatedUser.swift
+++ GitHub/AuthenticatedUser.swift
@@ -16,19 +16,39 @@ extension GitHub {
 		let login: String
 		let id: Int
 		let avatarURL: URL
+		let followersURL: URL
+		let followingURL: String
+		let starredURL: String
 		let reposURL: URL
 		let name: String?
+		let company: String?
+		let location: String?
 		let email: String?
 		let bio: String?
+		let publicReposCount: Int
+		let followersCount: Int
+		let followingCount: Int
+		let totalPrivateReposCount: Int
+		let ownedPrivateReposCount: Int
 
 		private enum CodingKeys: String, CodingKey {
 			case login
 			case id
 			case avatarURL = "avatar_url"
+			case followersURL = "followers_url"
+			case followingURL = "following_url"
+			case starredURL = "starred_url"
 			case reposURL = "repos_url"
 			case name
+			case company
+			case location
 			case email
 			case bio
+			case publicReposCount = "public_repos"
+			case followersCount = "followers"
+			case followingCount = "following"
+			case totalPrivateReposCount = "total_private_repos"
+			case ownedPrivateReposCount = "owned_private_repos"
 		}
 
 	}
